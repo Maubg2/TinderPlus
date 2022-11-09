accdf32cbaf0eb0bd07f51aa6cc1700d0472e862
Maubg2 <mauriciobel2002@gmail.com>
2022-11-09 11:41:39 -0500

    Prueba

--- src/co/edu/unbosque/controller/Main.java
+++ src/co/edu/unbosque/controller/Main.java
@@ -3,7 +3,8 @@ package co.edu.unbosque.controller;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola hpta");
 
 	}
 
