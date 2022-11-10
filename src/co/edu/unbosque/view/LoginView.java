5bb0c868a0ee57a8c99d4aca218fb8699da976c3
DevJohann <felipepoveda273@gmail.com>
2022-11-10 15:34:04 -0500

    loginView

--- src/co/edu/unbosque/view/LoginView.java
+++ src/co/edu/unbosque/view/LoginView.java
@@ -1,9 +1,49 @@
 package co.edu.unbosque.view;
 
-public class LoginView {
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginView extends JPanel{
+	
+	private JLabel loginTitle;
+	private JLabel userLabel;
+	private JTextField userField;
+	private JLabel psswdLabel;
+	private JTextField psswdField;
+	
 	
 	public LoginView() {
 		
+		setVisible(false);
+		
+		//Title
+		loginTitle = new JLabel("Iniciar sesión");
+		loginTitle.setFont(new Font("Times New Roman", Font.BOLD, 24));
+		loginTitle.setBounds(100, 20, 100, 80);
+		add(loginTitle);
+		
+		//User section
+		userLabel = new JLabel("Usuario: ");
+		userLabel.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		userLabel.setBounds(50, 100, 100, 80);
+		add(userLabel);
+		
+		userField = new JTextField();
+		userField.setBounds(80, 100, 80, 50);
+		add(userField);
+		
+		//Password section
+		psswdLabel = new JLabel("Contraseña: ");
+		psswdLabel.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		psswdLabel.setBounds(50, 200, 80, 50);
+		add(psswdLabel);
+		
+		psswdField = new JTextField();
+		psswdField.setBounds(100, 200, 80, 50);
+		add(psswdField);
 	}
 
 }
