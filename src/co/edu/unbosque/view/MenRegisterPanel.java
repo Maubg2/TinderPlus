8c0633a02171b6037a7def174f0f10ab3e00e417
Maubg2 <mauriciobel2002@gmail.com>
2022-11-11 14:19:13 -0500

    WomenView

--- src/co/edu/unbosque/view/MenRegisterPanel.java
+++ src/co/edu/unbosque/view/MenRegisterPanel.java
@@ -10,137 +10,35 @@ import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.LineBorder;
 
-public class MenRegisterPanel extends JPanel{
+public class MenRegisterPanel extends GeneralRegister{
 	
-	//Data
-	private JLabel title;
-	private JLabel usernameMenRegLabel;
-	private JTextField usernameMenRegField;
-	private JLabel ageMenRegLabel;
-	private JTextField ageMenRegField;
-	private JLabel mailMenRegLabel;
-	private JTextField mailMenRegField;
-	private JLabel heightMenRegLabel;
-	private JTextField heightMenRegField;
-	
-	//Buttons
-	private JButton exitMRVButton;
-	private JButton nextMRVButton;
+	private JLabel salaryMRPLabel;
+	private JTextField salaryMRPField;
 	
 	public MenRegisterPanel() {
 		
+		super();
 		setLayout(null);
 		setVisible(false);
 		setBackground(Color.decode("#FFA1CF"));
-		//Title section
-		title = new JLabel("Registrarse");
-		title.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		title.setBounds(220, 20, 190, 80);
-		add(title);
 		
-		//Username section
-		usernameMenRegLabel = new JLabel("Nombre completo: ");
-		usernameMenRegLabel.setBounds(42, 80, 120, 100);
-		usernameMenRegLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		add(usernameMenRegLabel);
+		salaryMRPLabel = new JLabel("Salario: ");
+		salaryMRPLabel.setBounds(108, 210, 100, 80);
+		salaryMRPLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		add(salaryMRPLabel);
 		
-		usernameMenRegField = new JTextField();
-		usernameMenRegField.setBounds(160, 118, 180, 24);
-		add(usernameMenRegField);
+		salaryMRPField = new JTextField();
+		salaryMRPField.setBounds(160, 240, 100, 24);
+		add(salaryMRPField);
 		
-		//Age section
-		ageMenRegLabel = new JLabel("Edad: ");
-		ageMenRegLabel.setBounds(120, 124, 80, 80);
-		ageMenRegLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		add(ageMenRegLabel);
-		
-		ageMenRegField = new JTextField();
-		ageMenRegField.setBounds(160, 150, 100, 24);
-		add(ageMenRegField);
-		
-		//Mail section
-		mailMenRegLabel = new JLabel("Correo: ");
-		mailMenRegLabel.setBounds(104, 154, 100, 80);
-		mailMenRegLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		add(mailMenRegLabel);
-		
-		mailMenRegField = new JTextField();
-		mailMenRegField.setBounds(160, 180, 100, 24);
-		add(mailMenRegField);
-		
-		//Height section
-		heightMenRegLabel = new JLabel("Altura: ");
-		heightMenRegLabel.setBounds(108, 182, 100, 80);
-		heightMenRegLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		add(heightMenRegLabel);
-		
-		heightMenRegField = new JTextField();
-		heightMenRegField.setBounds(160, 210, 100, 24);
-		add(heightMenRegField);
-		
-		//Buttons section
-		exitMRVButton = new JButton("Salir");
-		exitMRVButton.setBounds(40, 330, 80, 30);
-		exitMRVButton.setBackground(Color.decode("#EA047E"));
-		exitMRVButton.setBorder(new EtchedBorder(100));
-		exitMRVButton.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		exitMRVButton.setForeground(Color.WHITE);
-		add(exitMRVButton);
-		
-		nextMRVButton = new JButton("Terminar");
-		nextMRVButton.setBounds(440, 330, 120, 30);
-		nextMRVButton.setBackground(Color.decode("#EA047E"));
-		nextMRVButton.setBorder(new EtchedBorder(100));
-		nextMRVButton.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		nextMRVButton.setForeground(Color.WHITE);
-		add(nextMRVButton);
 	}
 
-	public JTextField getUsernameMenRegField() {
-		return usernameMenRegField;
+	public JTextField getSalaryMRPField() {
+		return salaryMRPField;
 	}
 
-	public void setUsernameMenRegField(JTextField usernameMenRegField) {
-		this.usernameMenRegField = usernameMenRegField;
+	public void setSalaryMRPField(JTextField salaryMRPField) {
+		this.salaryMRPField = salaryMRPField;
 	}
 
-	public JTextField getAgeMenRegField() {
-		return ageMenRegField;
-	}
-
-	public void setAgeMenRegField(JTextField ageMenRegField) {
-		this.ageMenRegField = ageMenRegField;
-	}
-
-	public JTextField getMailMenRegField() {
-		return mailMenRegField;
-	}
-
-	public void setMailMenRegField(JTextField mailMenRegField) {
-		this.mailMenRegField = mailMenRegField;
-	}
-
-	public JTextField getHeightMenRegField() {
-		return heightMenRegField;
-	}
-
-	public void setHeightMenRegField(JTextField heightMenRegField) {
-		this.heightMenRegField = heightMenRegField;
-	}
-
-	public JButton getExitMRVButton() {
-		return exitMRVButton;
-	}
-
-	public void setExitMRVButton(JButton exitMRVButton) {
-		this.exitMRVButton = exitMRVButton;
-	}
-
-	public JButton getNextMRVButton() {
-		return nextMRVButton;
-	}
-
-	public void setNextMRVButton(JButton nextMRVButton) {
-		this.nextMRVButton = nextMRVButton;
-	}
 }
