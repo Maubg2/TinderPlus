fc1513c08f99ff68f908cf9f87b01af5691f9cc4
DevJohann <felipepoveda273@gmail.com>
2022-11-11 13:03:04 -0500

    act

---
+++ src/co/edu/unbosque/model/AppDTO.java
@@ -0,0 +1,17 @@
+package co.edu.unbosque.model;
+
+/**
+ * 
+ * Clase que sirve de central de datos
+ * @author J&M Sistemas
+ *
+ */
+public class AppDTO {
+	
+	
+	
+	
+	public AppDTO() {
+		
+	}
+}
--- src/co/edu/unbosque/model/User.java
+++ src/co/edu/unbosque/model/User.java
@@ -1,5 +1,5 @@
 package co.edu.unbosque.model;
 
-public class test {
+public class User {
 
 }
